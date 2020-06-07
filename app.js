b87bce01026173f2710324bb95c1e5e3133f3474
Kevin Skayro <kev1n-1997@outlook.com>
2020-06-07 15:21:47 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+//Global selections and variables
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll(".color h2");
+//Functions
+
+function generateHex() {
+  const hexColor = chroma.random();
+  return hexColor;
+}
+
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+    checkTextContrast(randomColor, hexText);
+  });
+}
+randomColors();
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = "black";
+  } else {
+    text.style.color = "white";
+  }
+}
