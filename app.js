f0a1ff4645887460bfd5c7bbd90d90fd7d3a40fa
Kevin Skayro <kev1n-1997@outlook.com>
2020-06-08 10:41:54 -0400

    added the js color range to saturation and brightness

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //Global selections and variables
+
 const colorDivs = document.querySelectorAll(".color");
 const generateBtn = document.querySelector(".generate");
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll(".color h2");
+
 //Functions
 
 function generateHex() {
@@ -18,9 +20,16 @@ function randomColors() {
     div.style.backgroundColor = randomColor;
     hexText.innerText = randomColor;
     checkTextContrast(randomColor, hexText);
+    // Initial Colorize Sliders
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll(".sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(color, hue, brightness, saturation);
   });
 }
-randomColors();
 
 function checkTextContrast(color, text) {
   const luminance = chroma(color).luminance();
@@ -30,3 +39,20 @@ function checkTextContrast(color, text) {
     text.style.color = "white";
   }
 }
+
+function colorizeSliders(color, hue, brightness, saturation) {
+  //scale saturation
+  const noSat = color.set("hsl.s", 0);
+  const fullSat = color.set("hsl.s", 1);
+  const scaleSat = chroma.scale([noSat, color, fullSat]);
+
+  //scale brightness
+  const midBright = color.set("hsl.l", 0.5);
+  const scaleBright = chroma.scale(["black", midBright, "white"]);
+  //input update
+
+  saturation.style.backgroundImage = `linear-gradient(to right,${scaleSat(0)}, ${scaleSat(1)})`;
+  brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(0)}, ${scaleBright(0.5)}, ${scaleBright(1)})`;
+}
+
+randomColors();
