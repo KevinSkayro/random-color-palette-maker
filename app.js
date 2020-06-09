d1b5b5c3ee4dbf45d455226410d3358c09a815eb
Kevin Skayro <kev1n-1997@outlook.com>
2020-06-09 08:56:59 -0400

    added feature to change color with the sliders

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const generateBtn = document.querySelector(".generate");
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll(".color h2");
 
+//Event Listeners
+
+sliders.forEach((slider) => {
+  slider.addEventListener("input", hslControls);
+});
+
 //Functions
 
 function generateHex() {
@@ -54,6 +60,19 @@ function colorizeSliders(color, hue, brightness, saturation) {
   saturation.style.backgroundImage = `linear-gradient(to right,${scaleSat(0)}, ${scaleSat(1)})`;
   brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(0)}, ${scaleBright(0.5)}, ${scaleBright(1)})`;
   hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75), rgb(204,204,75), rgb(75,204,75), rgb(75,204,204), rgb(75,75,204), rgb(204,75,204), rgb(204,75,75))`;
+}
+function hslControls(e) {
+  const index = e.target.getAttribute("data-hue") || e.target.getAttribute("data-bright") || e.target.getAttribute("data-sat");
+
+  let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
+  const hue = sliders[0];
+  const brightness = sliders[1];
+  const saturation = sliders[2];
+
+  const bgColor = colorDivs[index].querySelector("h2").innerText;
+  let color = chroma(bgColor).set("hsl.s", saturation.value).set("hsl.l", brightness.value).set("hsl.h", hue.value);
+
+  colorDivs[index].style.backgroundColor = color;
 }
 
 randomColors();
